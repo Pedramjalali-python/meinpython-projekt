65f719d420abaa403c0427b6f894979f914789fb
Pedram jalali <159775663+Pedramjalali-python@users.noreply.github.com>
2024-08-13 04:21:49 -0700

    Create Scope.py

---
+++ Scope.py
@@ -0,0 +1,11 @@
+öffene_seiten = ["youtube", "easy german", "neonlearn.ir"]
+
+
+
+öffene_seiten.pop()
+print("zurück auf", öffene_seiten[-1])
+
+if not öffene_seiten:
+    print("keine öffene seiten mehr")
+
+print(öffene_seiten)
